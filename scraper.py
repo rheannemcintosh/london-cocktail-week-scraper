79991ebc469b2080f7d2b34481ffb4ec799268d9
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-11-01 00:26:43 +0000

    Store all the data in a data frame

--- scraper.py
+++ scraper.py
@@ -1,6 +1,10 @@
 # Import Statements
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+
+# Create the inital Data Frame
+df = pd.DataFrame(columns=['Index', 'Bar Name', 'Address', 'Description', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
 
 # Get the Request from the URL
 request = requests.get('https://londoncocktailweek.com/bars/print/?collectionId=0&whatId=0&areaId=0&spiritId=0&openNow=0&search=')
@@ -26,17 +30,27 @@ for i, child in enumerate(children):
     address = child.find('div', {'class': 'text'}).getText()
     opening_hours_container = child.find('ul', {'class': 'opening_hours__container'})
     opening_hours = opening_hours_container.find_all('li', {'class': 'opening_hours__times'})
-    for day in opening_hours:
+    times = []
+    for j, day in enumerate(opening_hours):
         day_of_week = day.find('div', {'class': 'text'}).getText()
         hours = day.find('li', {'class': 'text'}).getText()
     description = child.find('p', {'class': 'text--padded'}).getText()
+        times.append({j: hours})
+    
     bars.append(bar)
     addresses.append(address)
     descriptions.append(description)
+    
+    df.loc[i] = [i, bar, address, description, times[0], times[1], times[2], times[3], times[4], times[5], times[6]]
+
+
+    #print(opening_hours)
+    #print(bar)
+    #print(opening_hours)
 
 b = []
 for i, item in enumerate(bars):
     title = item.getText()
     b.append(title)
 
-print(b)
+print(b)print(df)
